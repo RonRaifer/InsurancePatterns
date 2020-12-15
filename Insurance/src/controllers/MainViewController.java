0de125115b262444496c213378837689a8758474
Ron Raifer <ronraifer@gmail.com>
2020-12-15 15:27:50 +0200

    added first scene

---
+++ Insurance/src/controllers/MainViewController.java
@@ -0,0 +1,61 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MainViewController {
+
+    @FXML
+    private AnchorPane apMainView;
+
+    @FXML
+    private Button btCarIns;
+
+    @FXML
+    private Button btApartmentIns;
+
+    @FXML
+    private Button btLifeIns;
+
+    @FXML
+    private Button btHealthIns;
+
+    @FXML
+    private Button btViewPurchases;
+
+    @FXML
+    private Button btSue;
+
+    @FXML
+    void ApartmentInsurance_btnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void CarInsurance_btnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HealthInsurance_btnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LifeInsurance_btnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Sue_btnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ViewPurchases_btnClick(ActionEvent event) {
+
+    }
+
+}
